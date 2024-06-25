d51677c9667a76c2e5b65010e0db26586bcf9ec3
Ting-Yu Chen <7ingyu@gmail.com>
2024-06-24 19:00:02 -0700

    login route added

--- server/controller/users.js
+++ server/controller/users.js
@@ -19,14 +19,28 @@ router.post('/register', async (req, res) => {
   }
 })
 
-// route.get('/login', async (req, res) => {
-//   try {
-//     const users = await User.find()
-//     res.json(users)
-//   } catch (error) {
-//     console.error(error)
-//     res.status(500).json({ message: 'Server Error' })
-//   }
-// })
+router.get('/login', async (req, res) => {
+  try {
+    const { email, password } = req.query
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (match) {
+      return res.json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      })
+    }
+    res.status(401).json({ message: 'Invalid credentials' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Server Error' })
+  }
+})
 
 module.exports = router
